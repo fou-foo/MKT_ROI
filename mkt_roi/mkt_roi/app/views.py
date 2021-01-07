72ae929757df843265678cc3f8e816ebf7e0c92f
José Antonio García <jose.ramirez@cimat.mx>
2021-01-07 16:59:00 -0600

    Update views.py

--- mkt_roi/mkt_roi/app/views.py
+++ mkt_roi/mkt_roi/app/views.py
@@ -43,25 +43,25 @@ def Menu(request):
     print('entro al menu')
 
 
-    if request.user.perfil.tipo=='medios_en_tienda' or request.user.perfil.tipo=='cenic' :
+    if request.user.perfil.tipo in ['medios_en_tienda' ] :#'cenic' :
         return render(request, 'app/medios_en_tienda_index.html', {'form': 1 })
         
-    if request.user.perfil.tipo=='medios_off_line' or request.user.perfil.tipo=='cenic' :
+    if request.user.perfil.tipo in ['medios_off_line'] : #'cenic' :
         return render(request, 'app/medios_off_line_index.html', {'form': 1 })
 
-    if request.user.perfil.tipo=='e-marketing' or request.user.perfil.tipo=='cenic' :
+    if request.user.perfil.tipo in ['e-marketing'] : #'cenic' :
         return render(request, 'app/e_marketing_index.html', {'form': 1 })
 
-    if request.user.perfil.tipo=='relaciones_publicas' or request.user.perfil.tipo=='cenic' :
+    if request.user.perfil.tipo in ['relaciones_publicas'] : #'cenic' :
         return render(request, 'app/relaciones_publicas_index.html', {'form': 1 })
 
-    if request.user.perfil.tipo=='mercadotecnia' or request.user.perfil.tipo=='cenic' :
+    if request.user.perfil.tipo in ['mercadotecnia'] : #'cenic' :
         return render(request, 'app/mercadotecnia_index.html', {'form': 1 })
 
-    if request.user.perfil.tipo=='mkt_digital' or request.user.perfil.tipo=='cenic' :
+    if request.user.perfil.tipo in ['mkt_digital'] :# 'cenic' 
         return render(request, 'app/mkt_digital_index.html', {'form': 1 })
 
-    if request.user.perfil.tipo=='medicion_digital' or request.user.perfil.tipo=='cenic' :
+    if request.user.perfil.tipo in ['medicion_digital'] : #'cenic' 
         return render(request, 'app/medicion_digital_index.html', {'form': 1 })
 
 
