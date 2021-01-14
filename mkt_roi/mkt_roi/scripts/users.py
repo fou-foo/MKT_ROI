be159a90eca2345f825d93ad7debfa2cb0838097
Antonio García <jose.ramirez@cimat.mx>
2021-01-13 20:05:57 -0600

    se comienza a trabajar en el front-end  para presentación del día jueves 14

--- mkt_roi/mkt_roi/scripts/users.py
+++ mkt_roi/mkt_roi/scripts/users.py
@@ -3,9 +3,18 @@ from app.models import Perfil
 def run():
     tipo = ['medios_off_line', 'medios_en_tienda', 'e-marketing', 'relaciones_publicas',  'mercadotecnia' ,
             'medicion_digital' , 'mkt_digital' , 'cenic' ]
-    for area in tipo:
+
+    nombre = [ 'Jorge Luis',       'Jazmín',    'Frank',           'Andrea' ,      'Juan David', 
+                'Víctor Hugo',  'Arturo', 'Dulce'] # cuidar orden con areas
+
+    apellido = [ 'Loaiza',          'Izabal',   ' Astorga',          'Parra' ,    'González',
+                 'Estrada Bello', 'Muñoz' , 'Magaña']         # cuidar orden con areas
+
+    for i in range(len(tipo)) :
+        area = tipo[i]
         print(area[::-1])
-        user = Perfil.objects.create_user( username=area, email='antonio.garciar@coppel.com', password=area[::-1], tipo=area )
+        user = Perfil.objects.create_user( username=area, email='antonio.garciar@coppel.com', password=area[::-1], tipo=area , first_name=nombre[i], 
+        last_name=apellido[i] )
         user.save()
        
     print(' Fin de creacion de users')
